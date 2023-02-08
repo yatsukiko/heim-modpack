f9bbb9faa84fb0105b03eb502601cb4a8321a5d9
yatsukiko <yatsu@yatsukiko.me>
2023-02-08 21:42:26 +0100

    close after execution

--- rest.bat
+++ rest.bat
@@ -1,6 +1,6 @@
 if exist BepInEx/plugins/warfare.dll (
-  already downloaded
+  echo already downloaded
 ) else (
 curl -o BepInEx/plugins/warfare.dll https://i.yatsukiko.me/XoCI8/peqiXEye38.dll/raw
 )
-valheim.exe
+start valheim.exe
